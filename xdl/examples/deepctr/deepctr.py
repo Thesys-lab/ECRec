6194acb32372487fca25fad4af26460c24889525
Kaige Liu <kaigel@andrew.cmu.edu>
2020-04-26 17:55:22 -0400

    update master experiment

--- xdl/examples/deepctr/deepctr.py
+++ xdl/examples/deepctr/deepctr.py
@@ -17,44 +17,40 @@ import tensorflow as tf
 import xdl
 
 reader = xdl.DataReader("r1", # name of reader
-                        paths=["./data.txt"], # file paths
+                        paths=["./generated_data.txt"], # file paths
                         enable_state=False) # enable reader state
 
-reader.epochs(1).threads(1).batch_size(10).label_count(1)
-reader.feature(name='sparse0', type=xdl.features.sparse)\
-    .feature(name='sparse1', type=xdl.features.sparse)\
-    .feature(name='deep0', type=xdl.features.dense, nvec=256)
+reader.epochs(1000).threads(4).batch_size(10).label_count(1)
+reader.feature(name='sparse0', type=xdl.features.sparse, serialized=True) \
+    #    .feature(name='sparse1', type=xdl.features.sparse, serialized=True)\
+#    .feature(name='deep0', type=xdl.features.dense, nvec=256)
 reader.startup()
 
 def train():
     batch = reader.read()
-    sess = xdl.TrainSession()
-    emb1 = xdl.embedding('emb1', batch['sparse0'], xdl.TruncatedNormal(stddev=0.001), 8, 1024, vtype='hash')
-    emb2 = xdl.embedding('emb2', batch['sparse1'], xdl.TruncatedNormal(stddev=0.001), 8, 1024, vtype='hash')
-    loss = model(batch['deep0'], [emb1, emb2], batch['label'])
+    emb1 = xdl.embedding('emb1', batch['sparse0'], xdl.Constant(0.0), 8, 4096, vtype='index')
+    loss = model([emb1], batch['label'])
     train_op = xdl.SGD(0.5).optimize()
     log_hook = xdl.LoggerHook(loss, "loss:{0}", 10)
     sess = xdl.TrainSession(hooks=[log_hook])
+    run_option = xdl.RunOption()
+    run_option.perf = True
+    run_statistic = xdl.RunStatistic()
     while not sess.should_stop():
-        sess.run(train_op)
+        sess.run(train_op, run_option, run_statistic)
+    xdl.Timeline(run_statistic.perf_result).save('./timeline.json')
+
 
 @xdl.tf_wrapper()
-def model(deep, embeddings, labels):
-    input = tf.concat([deep] + embeddings, 1)
+def model(embeddings, labels):
+    input = tf.concat(embeddings, 1)
     fc1 = tf.layers.dense(
         input, 128, kernel_initializer=tf.truncated_normal_initializer(
             stddev=0.001, dtype=tf.float32))
-    fc2 = tf.layers.dense(
-        fc1, 64, kernel_initializer=tf.truncated_normal_initializer(
-            stddev=0.001, dtype=tf.float32))
-    fc3 = tf.layers.dense(
-        fc2, 32, kernel_initializer=tf.truncated_normal_initializer(
-            stddev=0.001, dtype=tf.float32))
     y = tf.layers.dense(
-        fc3, 1, kernel_initializer=tf.truncated_normal_initializer(
+        fc1, 1, kernel_initializer=tf.truncated_normal_initializer(
             stddev=0.001, dtype=tf.float32))
     loss = tf.losses.sigmoid_cross_entropy(labels, y)
     return loss
 
-train()        
-
+train()
