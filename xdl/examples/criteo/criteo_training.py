5acd3202a946f72c9d73da22f30fc6094e4c6a53
Kaige Liu <kaigel@andrew.cmu.edu>
2020-08-28 20:34:35 -0400

    update criteo

--- xdl/examples/criteo/criteo_training.py
+++ xdl/examples/criteo/criteo_training.py
@@ -17,20 +17,23 @@ import tensorflow as tf
 import xdl
 import time
 
-reader = xdl.DataReader("r1", # name of reader
-                        paths=["./generated_data.txt"] * 10, # file paths
-                        enable_state=False) # enable reader state
-
-reader.epochs(1).threads(16).batch_size(2048).label_count(1)
-reader.feature(name='sparse0', type=xdl.features.sparse, serialized=True)\
-    .feature(name='dense0', type=xdl.features.dense, nvec=13)
-reader.startup()
+DATA_FILE = "/xdl_training_samples/data.txt"
+EMB_DIMENSION = 168644
 
 def train():
+    reader = xdl.DataReader("r1", # name of reader
+                            paths=[DATA_FILE] * 10, # file paths
+                            enable_state=False) # enable reader state
+
+    reader.epochs(1).threads(16).batch_size(2048).label_count(1)
+    reader.feature(name='sparse0', type=xdl.features.sparse, serialized=True) \
+        .feature(name='dense0', type=xdl.features.dense, nvec=13)
+    reader.startup()
+
     batch = reader.read()
     sess = xdl.TrainSession()
     #TODO: switch to uniform
-    emb1 = xdl.embedding('emb1', batch['sparse0'], xdl.UniformUnitScaling(factor=0.125), 64, 168644, vtype='index')
+    emb1 = xdl.embedding('emb1', batch['sparse0'], xdl.UniformUnitScaling(factor=0.125), 64, EMB_DIMENSION, vtype='index')
     loss = model_top(batch['dense0'], [emb1], batch['label'])
     train_op = xdl.SGD(0.1).optimize()
     log_hook = xdl.LoggerHook(loss, "loss:{0}", 10)
@@ -61,10 +64,12 @@ def model_top(deep, embeddings, labels):
     fc3 = next_layer(fc2, 512, 256)
     stddev = (2.0/(257))**0.5
     y = tf.layers.dense(
-            fc3, 1, kernel_initializer=tf.truncated_normal_initializer(
-                stddev=stddev, dtype=tf.float32))
+        fc3, 1, kernel_initializer=tf.truncated_normal_initializer(
+            stddev=stddev, dtype=tf.float32))
     loss = tf.losses.sigmoid_cross_entropy(labels, y)
     return loss
 
-train()        
+
+if __name__ == "__main__":
+    train()
 
