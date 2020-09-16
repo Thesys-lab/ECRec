3c8c0931559f0d46c2aa9fc490c24ef8176fb704
Kaige Liu <kaigel@andrew.cmu.edu>
2020-09-16 19:48:07 -0400

    update criteo training

--- xdl/examples/criteo/criteo_training.py
+++ xdl/examples/criteo/criteo_training.py
@@ -16,17 +16,53 @@
 import tensorflow as tf
 import xdl
 import time
+import threading
+import sys
 
 DATA_FILE = "/xdl_training_samples/data.txt"
 EMB_DIMENSION = 187767405
-NUM_WORKERS = 9
+NUM_WORKERS = 15
+CKPT = False
+
+
+# TODO: measure and change these two to match 4 hours and 1h/30min ckpt interval
+TOTAL_NUM_STEPS = 62208
+TOTAL_NUM_CKPTS = 4
+CKPT_INTERVAL_NUM_STEPS = TOTAL_NUM_STEPS/TOTAL_NUM_CKPTS
+
+step = 0
+prev_step = 0
+stop_report = False
+report_interval = 10
+start = 0.0
+
+def my_print(x):
+    print(x)
+    sys.stdout.flush()
+
+
+def report_step_change():
+    report_count = 0
+    global step
+    global prev_step
+    global stop_report
+    global report_interval
+    global start
+    while not stop_report:
+        diff = step - prev_step
+        prev_step = step
+        my_print("({rc}, {diff}, {t}, {step})".format(diff=diff, rc=report_count, t=(time.time() - start), step=step))
+        target_next_print = report_interval * report_count
+        time.sleep(max(target_next_print + start - time.time(), 0))
+        report_count += 1
+
 
 def train():
     reader = xdl.DataReader("r1", # name of reader
                             paths=[DATA_FILE] * NUM_WORKERS, # file paths
                             enable_state=False) # enable reader state
 
-    reader.epochs(1).threads(16).batch_size(2048).label_count(1)
+    reader.epochs(100).threads(16).batch_size(2048).label_count(1)
     reader.feature(name='sparse0', type=xdl.features.sparse, serialized=True) \
         .feature(name='dense0', type=xdl.features.dense, nvec=13)
     reader.startup()
@@ -37,16 +73,42 @@ def train():
     emb1 = xdl.embedding('emb1', batch['sparse0'], xdl.UniformUnitScaling(factor=0.125), 64, EMB_DIMENSION, vtype='index')
     loss = model_top(batch['dense0'], [emb1], batch['label'])
     train_op = xdl.SGD(0.1).optimize()
-    log_hook = xdl.LoggerHook(loss, "loss:{0}", 10)
 
-    print("Starting time measurement")
+    my_print("Starting time measurement")
+
+    global step
+    global prev_step
+    global stop_report
+    global report_interval
+    global start
+
     start = time.time()
-    sess = xdl.TrainSession(hooks=[log_hook])
+    sess = xdl.TrainSession()
+
+    # run for one op first
+    sess.run(train_op)
+    t = threading.Thread(target=report_step_change)
+    t.start()
+
     while not sess.should_stop():
+        if step >= TOTAL_NUM_STEPS:
+            break
         sess.run(train_op)
+        step += 1
+        cur_time = time.time()
+
+        # CKPT
+        if CKPT and int(xdl.get_task_index()) and step % CKPT_INTERVAL_NUM_STEPS == 0:
+            my_print("Taking ckpt {v} starting {s}".format(v=ckpt_version, s=cur_time - start))
+            saver = xdl.Saver()
+            saver.save(version = str(step))
+            after_time =time.time()
+            my_print("Ending ckpt {v} starting {s}. Ckpt takes: {t}".format(v=ckpt_version, s=after_time - start, t=after_time-cur_time))
+    stop_report = True
+    t.join()
     end = time.time()
-    print("TOTAL TIME:!!!!!!!!!!!!!!!!!!!")
-    print(end - start)
+    my_print("TOTAL TIME:!!!!!!!!!!!!!!!!!!!")
+    my_print(end - start)
 
 @xdl.tf_wrapper()
 def model_top(deep, embeddings, labels):
@@ -73,4 +135,3 @@ def model_top(deep, embeddings, labels):
 
 if __name__ == "__main__":
     train()
-
