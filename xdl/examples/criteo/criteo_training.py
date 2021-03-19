6bd3129d2b29cfe0a8faa469f061d7436146fd7a
Tianyu Zhang <tianyuz2@andrew.cmu.edu>
2021-03-19 11:52:54 +0000

    fix emb table size to fit mem

--- xdl/examples/criteo/criteo_training.py
+++ xdl/examples/criteo/criteo_training.py
@@ -20,7 +20,8 @@ import threading
 import sys
 
 DATA_FILE = "/xdl_training_samples/data.txt"
-EMB_DIMENSION = 197767405
+# EMB_DIMENSION = 197767405
+EMB_DIMENSION = 19776740
 NUM_COPIES = 297
 CKPT = False
 TOTAL_NUM_STEPS = 226004
